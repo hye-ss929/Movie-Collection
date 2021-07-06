21cb8fe119802a6bb2872c6e202a1c41ec55df70
hyesung.heo <hye.ss929@gmail.com>
2021-07-06 21:49:56 +0900

    ADD: footer 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,9 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <FooterBox>
-      <FooterText></FooterText>
+      <FooterText>
+        <Icon src="/images/movie.png" alt="icon" />
+      </FooterText>
     </FooterBox>
   );
 };
@@ -13,11 +15,18 @@ export default Footer;
 
 const FooterBox = styled.footer`
   width: 100%;
-  /* height: 15px; */
   background-color: #fddf46;
 `;
 
 const FooterText = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 50px;
+`;
+
+const Icon = styled.img`
+  width: 38px;
+  height: 38px;
 `;
